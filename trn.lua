82e8df379e6a385aca2ac9038dab6299c4b4ac71
Arabic Deconstructor <arabicdeconstructor@gmail.com>
2019-12-29 18:53:13 -0800

    attempting to read tables from files.

--- trn.lua
+++ trn.lua
@@ -1,19 +1,18 @@
 function Span (elem)
   if elem.classes[1] == 'trn' then
-    print(elem.content)
     for index,text in pairs(elem.content) do
       for index2,text2 in pairs(text) do
-        print(text2)
-        print("\n")
+        text2 = string.gsub(text2, "v", "th")
+        text2 = string.gsub(text2, "x", "kh")
+        text2 = string.gsub(text2, "p", "dh")
+        text2 = string.gsub(text2, "c", "sh")
+        text2 = string.gsub(text2, "P", "Dh")
+        text2 = string.gsub(text2, "e", "ɛ")
+        text2 = string.gsub(text2, "g", "gh")
+        text2 = string.gsub(text2, "'", "ʾ")
         text2 = string.gsub(text2, "A", "aa")
         text2 = string.gsub(text2, "I", "ee")
         text2 = string.gsub(text2, "U", "oo")
-        text2 = string.gsub(text2, "c", "sh")
-        text2 = string.gsub(text2, "p", "dh")
-        text2 = string.gsub(text2, "P", "Dh")
-        text2 = string.gsub(text2, "v", "th")
-        text2 = string.gsub(text2, "'", "ʾ")
-        text2 = string.gsub(text2, "e", "ɛ")
 	text[index2] = text2
       end
       elem.content[index] = text
@@ -25,8 +24,4 @@ function Span (elem)
   else
     return elem
   end
-end
-
-function Emph (elem)
-  return pandoc.Strong(elem.content)
 end
