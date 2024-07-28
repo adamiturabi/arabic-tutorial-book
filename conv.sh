86634b29c3d62669f72e75594c7fe46104c4bdfa
Ahmad Sheikh <ahmadzsheikh@gmail.com>
2024-07-27 21:45:57 -0500

    replaced png files with postprocessed svg files

--- conv.sh
+++ conv.sh
@@ -3,8 +3,16 @@ cd Learn-Standard-Arabic_files/figure-latex/
 for FILE in *.pdf; do
   echo "${FILE%.*}"
   echo "${FILE#*.}"
+
   # density tune value from 100 to 150 to adjust size in html
-  magick -density 130 -quality 100 $FILE "${FILE%.*}.png"
-  mv -f "${FILE%.*}.png" ../../docs/Learn-Standard-Arabic_files/figure-html/
+  #magick -density 130 -quality 100 $FILE "${FILE%.*}.png"
+  #mv -f "${FILE%.*}.png" ../../docs/Learn-Standard-Arabic_files/figure-html/
+
+  dvisvgm --font-format=ttf --scale=1.45 --pdf $FILE
+  mv -f "${FILE%.*}.svg" ../../docs/Learn-Standard-Arabic_files/figure-html/
 done
+cd ../../docs/
+sed -i 's/\.png\"/.svg"/g' *.html
+cd Learn-Standard-Arabic_files/figure-html/
+rm *.png
 
--- docs/index.md
+++ docs/index.md
@@ -2,7 +2,7 @@
 title: "Learn Standard Arabic"
 subtitle:  "A self-instruction textbook with grammar, vocabulary, and exercises"
 author: "Author Names"
-date: "v0.1.0-715-g376f0ba"
+date: "v0.1.0-716-gbe09250"
 documentclass: book
 geometry:
 # A4 2 pages per sheet draft
