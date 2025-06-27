aea37f25c7b740a64cdc930c00caa6e191dca9c3
adamiturabi <adamiturabi@adamiturabi-MacBook-Pro.local>
2025-06-27 19:48:26 +0500

    separated lingex filter from arabic div

--- _extensions/arabic-support/arabic-div.lua
+++ _extensions/arabic-support/arabic-div.lua
@@ -1,5 +1,6 @@
 -- Add attributes for Arabic text in a div
-function ArabicDiv (el)
+Div = function(el)
+  if el.classes:includes 'ar' or el.classes:includes 'aralt' then
     contents = el.content
     if FORMAT:match 'latex' then
       -- for handling alternate Arabic font
@@ -21,61 +22,6 @@ function ArabicDiv (el)
       -- dir needed for html otherwise punctuation gets messed up
       return pandoc.Div(contents, {class=classval, lang='ar', dir='rtl'})
     end
-end
-ex_counter = 1
-
-function LingEx (el)
-    ex_counter_str = tostring(ex_counter)
-    ex_counter = ex_counter + 1
-
-    local has_ref = false
-    for i, item in ipairs(el.content) do
-      --if item.classes ~= nil and item.classes:includes "exref" then
-      if item.attributes ~= nil and item.attributes[1] == "ref" then
-        has_ref = true
-        -- if FORMAT:match 'latex' then
-        --   table.insert(
-        --     item.content, 1,
-        --     pandoc.RawInline('latex', '{\\footnotesize ')
-        --   )
-        --   table.insert(
-        --     item.content,
-        --     pandoc.RawInline('latex', '}')
-        --   )
-        -- end
-      end
-    end
-        
-    if FORMAT:match 'latex' then
-      local num_div = pandoc.Div(pandoc.RawInline('latex', '\\stepcounter{mycounter}(\\themycounter)') , {"num"})
-      --num_div.attributes['layout-valign'] = 'top' -- does not work: https://github.com/quarto-dev/quarto-cli/issues/9109
-      table.insert(
-        el.content, 1,
-        num_div
-      )
-    else
-      table.insert(
-        el.content, 1,
-        pandoc.Div("(" .. ex_counter_str .. ")", {"num"})
-      )
-    end
-    if not has_ref then
-      table.insert(
-        el.content, 
-        pandoc.Div("", {"ref"})
-      )
-    end
-
-    --return pandoc.Div(el.content, {layout='[0.1, 0.6, -0.1, 0.2]', fig_pos="H"})
-    local return_div = pandoc.Div(el.content, {layout='[0.1, 0.6, -0.1, 0.2]'})
-    return_div.attributes['fig-pos'] = 'H'
-    return return_div
-end
-
-function Div (el)
-  if el.classes:includes 'ar' or el.classes:includes 'aralt' then
-    return ArabicDiv(el)
-  elseif el.classes:includes 'lingex' then
-    return LingEx(el)
   end
 end
+
