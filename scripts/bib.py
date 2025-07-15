d46e8abe74bf0de15f4881ef4cbb524de8faf794
adamiturabi <adamiturabi@adamiturabi-MacBook-Pro.local>
2025-07-16 00:58:22 +0500

    added resources

--- scripts/bib.py
+++ scripts/bib.py
@@ -1,6 +1,6 @@
 from BibResource import BibResource
 
-def format_url_for_ar_div(url):
+def format_url_ar(url):
   return "[[" + url + "](" + url + ")]{lang='en'}"
 
 def populate_resource_list():
@@ -10,191 +10,211 @@ def populate_resource_list():
   resource_list.append(BibResource(
     "bukhari"
     , cit_text = "[صحيح البخاري]{.ar}"
-    , bib_text = "صحيح البخاري، " + format_url_for_ar_div("https://sunnah.com/bukhari")
+    , bib_text = "صحيح البخاري، " + format_url_ar("https://sunnah.com/bukhari")
     ))
 
   resource_list.append(BibResource(
     "muslim"
     , cit_text = "[صحيح مسلم]{.ar}"
-    , bib_text = "صحيح مسلم، " + format_url_for_ar_div("https://sunnah.com/muslim")
+    , bib_text = "صحيح مسلم، " + format_url_ar("https://sunnah.com/muslim")
     ))
 
   resource_list.append(BibResource(
     "nasai"
     , cit_text = "[سنن النسائي]{.ar}"
-    , bib_text = "[سنن النسائي]{.ar}، " + format_url_for_ar_div("https://sunnah.com/nasai")
+    , bib_text = "سنن النسائي، " + format_url_ar("https://sunnah.com/nasai")
     , sort_key = "سنن النسايي"
     ))
  
   resource_list.append(BibResource(
     "abudawud"
     , cit_text = "[سنن أبي داود]{.ar}"
-    , bib_text = "[سنن أبي داود]{.ar}، " + format_url_for_ar_div("https://sunnah.com/abudawud")
+    , bib_text = "سنن أبي داود، " + format_url_ar("https://sunnah.com/abudawud")
     , sort_key = "سنن ابي داود"
     ))
  
   resource_list.append(BibResource(
     "tirmidhi"
     , cit_text = "[جامع الترمذي]{.ar}"
-    , bib_text = "[جامع الترمذي]{.ar}، " + format_url_for_ar_div("https://sunnah.com/tirmidhi")
+    , bib_text = "جامع الترمذي، " + format_url_ar("https://sunnah.com/tirmidhi")
     ))
  
   resource_list.append(BibResource(
     "ibnmajah"
     , cit_text = "[سنن ابن ماجه]{.ar}"
-    , bib_text = "[سنن ابن ماجه]{.ar}، " + format_url_for_ar_div("https://sunnah.com/ibnmajah")
+    , bib_text = "سنن ابن ماجه، " + format_url_ar("https://sunnah.com/ibnmajah")
     ))
  
   resource_list.append(BibResource(
     "malik"
     , cit_text = "[موطأ مالك]{.ar}"
-    , bib_text = "[موطأ مالك]{.ar}، " + format_url_for_ar_div("https://sunnah.com/malik")
+    , bib_text = "موطأ مالك، " + format_url_ar("https://sunnah.com/malik")
     , sort_key = "موطا مالك"
     ))
  
   resource_list.append(BibResource(
     "ahmad"
     , cit_text = "[مسند أحمد]{.ar}"
-    , bib_text = "[مسند أحمد]{.ar}، " + format_url_for_ar_div("https://sunnah.com/ahmad")
+    , bib_text = "مسند أحمد، " + format_url_ar("https://sunnah.com/ahmad")
     , sort_key = "مسند احمد"
     ))
  
   resource_list.append(BibResource(
     "darimi"
     , cit_text = "[سنن الدارمي]{.ar}"
-    , bib_text = "[سنن الدارمي]{.ar}، " + format_url_for_ar_div("https://sunnah.com/darimi")
+    , bib_text = "سنن الدارمي، " + format_url_ar("https://sunnah.com/darimi")
     ))
  
   resource_list.append(BibResource(
     "nawawi40"
     , cit_text = "[الأربعون النووية]{.ar}"
-    , bib_text = "[الأربعون النووية]{.ar}، " + format_url_for_ar_div("https://sunnah.com/nawawi40")
+    , bib_text = "الأربعون النووية، " + format_url_ar("https://sunnah.com/nawawi40")
     , sort_key = "اربعون النووية"
     ))
  
   resource_list.append(BibResource(
     "riyadussalihin"
     , cit_text = "[رياض الصالحين]{.ar}"
-    , bib_text = "[رياض الصالحين]{.ar}، " + format_url_for_ar_div("https://sunnah.com/riyadussalihin")
+    , bib_text = "رياض الصالحين، " + format_url_ar("https://sunnah.com/riyadussalihin")
     ))
  
   resource_list.append(BibResource(
     "adab"
     , cit_text = "[الأدب المفرد]{.ar}"
-    , bib_text = "[الأدب المفرد]{.ar}، " + format_url_for_ar_div("https://sunnah.com/adab")
+    , bib_text = "الأدب المفرد، " + format_url_ar("https://sunnah.com/adab")
     , sort_key = "ادب المفرد"
     ))
  
   resource_list.append(BibResource(
     "shamail"
     , cit_text = "[الشمائل المحمدية]{.ar}"
-    , bib_text = "[الشمائل المحمدية]{.ar}، " + format_url_for_ar_div("https://sunnah.com/shamail")
+    , bib_text = "الشمائل المحمدية، " + format_url_ar("https://sunnah.com/shamail")
     , sort_key = "شمايل المحمدية"
     ))
  
   resource_list.append(BibResource(
     "mishkat"
     , cit_text = "[مشكاة المصابيح]{.ar}"
-    , bib_text = "[مشكاة المصابيح]{.ar}، " + format_url_for_ar_div("https://sunnah.com/mishkat")
+    , bib_text = "مشكاة المصابيح، " + format_url_ar("https://sunnah.com/mishkat")
     ))
  
   resource_list.append(BibResource(
     "bulugh"
     , cit_text = "[بلوغ المرام]{.ar}"
-    , bib_text = "[بلوغ المرام]{.ar}، " + format_url_for_ar_div("https://sunnah.com/bulugh")
+    , bib_text = "بلوغ المرام، " + format_url_ar("https://sunnah.com/bulugh")
     ))
  
   resource_list.append(BibResource(
     "forty"
     , cit_text = "[الأربعينات]{.ar}"
-    , bib_text = "[الأربعينات]{.ar}، " + format_url_for_ar_div("https://sunnah.com/forty")
+    , bib_text = "الأربعينات، " + format_url_ar("https://sunnah.com/forty")
     , sort_key = "اربعينات"
     ))
  
   resource_list.append(BibResource(
     "hisn"
     , cit_text = "[حصن المسلم]{.ar}"
-    , bib_text = "[حصن المسلم]{.ar}، " + format_url_for_ar_div("https://sunnah.com/hisn")
+    , bib_text = "حصن المسلم، " + format_url_ar("https://sunnah.com/hisn")
     ))
 
   # Tafsirs
   resource_list.append(BibResource(
     "albahr-almuheet"
     , cit_text = "[البحر المحيط لأبي حيان]{.ar}"
-    , bib_text = "[البحر المحيط، تأليف: أبو حيان]{.ar}، " + format_url_for_ar_div("https://tafsir.app")
+    , bib_text = "البحر المحيط، تأليف: أبو حيان، " + format_url_ar("https://tafsir.app")
     , sort_key = "بحر المحيط"
     ))
  
   resource_list.append(BibResource(
     "ibn-aashoor"
     , cit_text = "[تفسير ابن عاشور]{.ar}"
-    , bib_text = "[تفسير التحرير والتنوير، تأليف: ابن عاشور]{.ar}، " + format_url_for_ar_div("https://tafsir.app")
+    , bib_text = "تفسير التحرير والتنوير، تأليف: ابن عاشور، " + format_url_ar("https://tafsir.app")
     , sort_key = "تفسير التحرير والتنوير"
     ))
  
   resource_list.append(BibResource(
     "tabari"
     , cit_text = "[تفسير الطبري]{.ar}"
-    , bib_text = "[تفسير الطبري]{.ar}، " + format_url_for_ar_div("https://tafsir.app")
+    , bib_text = "تفسير الطبري، " + format_url_ar("https://tafsir.app")
     ))
  
   resource_list.append(BibResource(
     "ibn-katheer"
     , cit_text = "[تفسير ابن كثير]{.ar}"
-    , bib_text = "[تفسير ابن كثير]{.ar}، " + format_url_for_ar_div("https://tafsir.app")
+    , bib_text = "تفسير ابن كثير، " + format_url_ar("https://tafsir.app")
     ))
  
   resource_list.append(BibResource(
     "ibn-alqayyim"
     , cit_text = "[تفسير ابن القيم]{.ar}"
-    , bib_text = "[تفسير ابن القيم]{.ar}، " + format_url_for_ar_div("https://tafsir.app")
+    , bib_text = "تفسير ابن القيم، " + format_url_ar("https://tafsir.app")
     ))
  
   resource_list.append(BibResource(
     "ibn-taymiyyah"
     , cit_text = "[تفسير ابن تيمية]{.ar}"
-    , bib_text = "[تفسير ابن تيمية]{.ar}، " + format_url_for_ar_div("https://tafsir.app")
+    , bib_text = "تفسير ابن تيمية، " + format_url_ar("https://tafsir.app")
     ))
  
   resource_list.append(BibResource(
     "ibn-uthaymeen"
     , cit_text = "[تفسير ابن عثيمين]{.ar}"
-    , bib_text = "[تفسير ابن عثيمين]{.ar}، " + format_url_for_ar_div("https://tafsir.app")
+    , bib_text = "تفسير ابن عثيمين، " + format_url_ar("https://tafsir.app")
     ))
  
   resource_list.append(BibResource(
     "qurtubi"
     , cit_text = "[تفسير القرطبي]{.ar}"
-    , bib_text = "[تفسير القرطبي]{.ar}، " + format_url_for_ar_div("https://tafsir.app")
+    , bib_text = "تفسير القرطبي، " + format_url_ar("https://tafsir.app")
     ))
 
   # General
   resource_list.append(BibResource(
     "nahw_wafi"
     , cit_text = "[النحو الوافي]{.ar}"
-    , bib_text = "[النحو الوافي، تأليف: عباس حسن، دار المعارف.]{.ar}"
-    , sort_key = "[نحو الوافي]{.ar}"
+    , bib_text = "النحو الوافي، تأليف: عباس حسن، دار المعارف."
+    , sort_key = "نحو الوافي"
     ))
   
   resource_list.append(BibResource(
     "maani_nahw"
     , cit_text = "[معاني النحو]{.ar}"
-    , bib_text = "[معاني النحو، تأليف: فاضل صالح السامرائي. الطبعة الثالثة، دار ابن كثير، 2022\\ م.]{.ar}"
+    , bib_text = "معاني النحو، تأليف: فاضل صالح السامرائي. الطبعة الثالثة، دار ابن كثير، 2022\\ م."
     ))
   
   resource_list.append(BibResource(
     "nahw_arabi"
     , cit_text = "[النحو العربي: أحكام ومعان]{.ar}"
-    , bib_text = "[النحو العربي: أحكام ومعان، تأليف: فاضل صالح السامرائي. الطبعة الأولى، دار ابن كثير، 2014\\ م.]{.ar}"
-    , sort_key = "[نحو العربي احكام ومعان]{.ar}"
+    , bib_text = "النحو العربي: أحكام ومعان، تأليف: فاضل صالح السامرائي. الطبعة الأولى، دار ابن كثير، 2014\\ م."
+    , sort_key = "نحو العربي احكام ومعان"
     ))
   
   resource_list.append(BibResource(
     "radiy_kafiyah"
     , cit_text = "[شرح الرضي على الكافية]{.ar}"
-    , bib_text = "[شرح الكافية في النحو، تأليف: رضي الدين الأستراباذي (ت: 686\\ هـ).]{.ar}"
+    , bib_text = "شرح الكافية في النحو، تأليف: رضي الدين الأستراباذي (ت: 686\\ هـ)."
     ))
-  
+
+  resource_list.append(BibResource(
+    "ibn_ya3ish_mufassal"
+    , cit_text = "[شرح ابن يعيش على المفصل]{.ar}"
+    , bib_text = "شرح ابن يعيش على المفصل، قدم له الدكتور إميل بديع يعقوب، دار الكتب العلمية، بيروت، الطبعة الأولى، ١٤٢٢\\ هـ - ٢٠٠١\\ م" + format_url_ar("https://shamela.ws/book/13301")
+    ))
+
+  resource_list.append(BibResource(
+    "qawa3id_shaykh_zadeh"
+    , cit_text = "[شرح شيخ زاده على قواعد الإعراب]{.ar}"
+    , bib_text = "شرح قواعد الإعراب، تأليف: محمد بن مصطفى القُوجَوي المعروف بشيخ زاده (ت ٩٥٠ هـ)، دراسة وتحقيق: إسماعيل إسماعيل مروة، دار الفكر المعاصر، بيروت، الطبعة الأولى، ١٤١٦\\ هـ - ١٩٩٥\\ م" + format_url_ar("https://shamela.ws/book/19236")
+    , sort_key = "شرح شيخ زاده على قواعد الاعراب"
+    ))
+
+  resource_list.append(BibResource(
+    "ibn_aqil_alfiyyah"
+    , cit_text = "[شرح ابن عقيل على الألفية]{.ar}"
+    , bib_text = "شرح ابن عقيل على الألفية، دار التراث - القاهرة، الطبعة العشرون ١٤٠٠\\ هـ - ١٩٨٠\\ م."+format_url_ar("https://shamela.ws/book/9904")
+    , sort_key = "[شرح ابن عقيل على الالفية]{.ar}"
+    ))
+
   resource_list.append(BibResource(
     "wright"
     , cit_text = "Wright"
@@ -212,6 +232,13 @@ def populate_resource_list():
     , cit_text = "Sadan,\\ A., _The Subjunctive Mood in Arabic Grammatical Thought_"
     , bib_text = "Sadan,\\ A., _The Subjunctive Mood in Arabic Grammatical Thought_, Brill, 2012. <https://doi.org/10.1163/9789004234239>"
     , sort_key = "Sadan A Subjunctive Mood in Arabic Grammatical Thought"
+    ))
+
+  resource_list.append(BibResource(
+    "jallad_wawation"
+    , cit_text = 'Al-Jallad,\\ A., "One wāw to rule them all: The origins and fate of wawation in Arabic and its orthography"'
+    , bib_text = 'Al-Jallad,\\ A., "One wāw to rule them all: The origins and fate of wawation in Arabic and its orthography," in: _Scripts and scripture: Writing and religion in Arabia circa 500-700\\ CE_, pp.\\ 87--104, 2022'
+    , sort_key = "Jallad A One waw to rule them all"
     ))
 
   return resource_list
