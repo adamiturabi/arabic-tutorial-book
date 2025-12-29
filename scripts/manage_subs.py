eef88ad3d69f63142cbbece6890fbbab2754e1d7
adamiturabi <adamiturabi@adamiturabi-MacBook-Pro.local>
2025-12-29 11:26:12 +0500

    moved pre-render subs to a python script instead of a lua filter. decommissioned filters/subs.lua

---
+++ scripts/manage_subs.py
@@ -0,0 +1,189 @@
+
+map_table = {}
+
+# symbols
+map_table["x"] = "✗"
+map_table["v"] = "✓"
+map_table["rarr"] = "→"
+map_table["larr"] = "←"
+
+# text terms
+map_table["ia"] = "if Allāh wills"
+map_table["quran"] = "Qurʾān"
+map_table["ayah"] = "[Ayah]{.trn2}"
+map_table["surah"] = "[sUrah]{.trn2}"
+map_table["tafsir"] = "[tafsIr]{.trn2}"
+
+# 3aaqil
+map_table["3aaqil"]   = "intelligent being"   # raised-state"
+map_table["g_3aaqil"] = "non-intelligent being"  # propped-state
+
+# states
+map_table["ustate"] = "u-state"   # raised-state"
+map_table["astate"] = "a-state"  # propped-state
+map_table["istate"] = "i-state"  # lowered-state
+map_table["0state"] = "0-state"  # clipped-state
+
+# diacritics
+map_table["amark"] = "[fatHah]{.trn2}"
+map_table["imark"] = "[kasrah]{.trn2}"
+map_table["umark"] = "[Dammah]{.trn2}"
+map_table["0mark"] = "[sukUn]{.trn2}"
+map_table["shaddah"] = "[caddah]{.trn2}"
+map_table["tanwin"] = "[tanwIn]{.trn2}"
+
+# person
+map_table["first"] = "speaker"
+map_table["second"] = "adressee"
+map_table["third"] = "absentee"
+
+# mabny and mutasarrif
+map_table["mabny"] = "rigid"
+map_table["diptote"] = "semi-flexible"
+map_table["triptote"] = "fully-flexible"
+map_table["tote"] = "flexible"
+
+# active and passive voice
+map_table["active"] = "active" # "known-doer" verb?
+map_table["passive"] = "passive" # "unknown-doer" verb?
+
+# government
+map_table["3aamil"] = "governor"
+map_table["ma3mul"] = "governee"
+map_table["3amal"] = "government"
+map_table["3umdah"] = "structural" # indispensable, foundational, basic, load-bearing, structural
+map_table["fadlah"] = "dispensable" # dispensable, redundant, extra
+
+# jumlah khabariyyah and inshaa'iyyah
+map_table["xabari"] = "truth-evaluable" # falsifiable
+map_table["inshai"] = "non-truth-evaluable" # non-falsifiable
+
+# idaafah types
+map_table["ma3nawi"] = "real" 
+map_table["lafzi"] = "superficial" # false, superficial
+
+# alphabetical
+map_table["3aaid"] = "refer-back pronoun"
+map_table["3atf"] = "conjunction"
+map_table["3atf_bayan"] = "clarifying follower"
+
+map_table["adat_istifham"] = "question word"
+map_table["af3al"] = "comparative noun"
+map_table["af3aal_quloob"] = "verbs of perception"
+
+map_table["badal"] = "substitute" # replacement, substitute
+
+map_table["damir_shan"] = "state-of-affairs pronoun" # pronoun-of-fact, pronoun of s͡haʾn
+map_table["dharf_makan"] = "adverb of place"
+map_table["dharf_zaman"] = "adverb of time"
+
+map_table["faa3il"] = "doer"
+
+map_table["hal"] = "[HAl]{.trn2}"
+
+map_table["i3timaad"] = "governing support"
+map_table["idaafah"] = "annexation"
+map_table["indir_mafulb"] = "indirect doee"
+map_table["pregov"] = "foundational government" # initial gov, pre-gov, foundational gov
+map_table["ism_fail"] = "doer participle"
+map_table["ism_isharah"] = "pointing noun"
+map_table["ism_of"] = "subject"
+map_table["ism_maful"] = "doee participle"
+map_table["ism_marrah"] = "one-time noun"
+map_table["ism_hay2ah"] = "noun of kind"
+map_table["ism_makan"] = "noun of place"
+map_table["ism_masdar"] = "quasi-maṣdar"
+map_table["ism_mawsul"] = "connected noun"
+map_table["ism_zaman"] = "noun of time"
+map_table["isnaad_comp"] = "structurally complete"
+
+map_table["jamid"] = "underived"
+map_table["jawab"] = "consequence"
+map_table["jawab_shart"] = "condition-response"
+map_table["j_ism"] = "nounal sentence"
+map_table["j_f3l"] = "verbal sentence"
+
+map_table["laam_taqwiya"] = "strengthening [ل]{.ar}"
+map_table["lazim"] = "intransitive"
+map_table["lnj"] = {"لا النافية للجنس", "ar"}
+
+map_table["ma3toof"] = "conjunctee"  # "stateless" verb?
+map_table["madi"] = "perfect"  # "stateless" verb?
+map_table["maful"] = "doee"
+map_table["mafulb"] = "direct doee"
+map_table["mafull"] = "adverb of reason"
+map_table["mafulm"] = "accompanying doee"
+map_table["manut"] = "attributee"
+map_table["masdar"] = "[maSdar]{.trn2}"
+map_table["masdari_an"] = "[maSdari Ean]{.trn2}"
+map_table["mmutlaq"] = "absolute doee"
+map_table["mubdalb"] = "substitutee" # replacee, substitutee
+map_table["mubtada"] = "subject"
+map_table["mudaf"] = "annexe noun"
+map_table["mudafil"] = "base noun"
+map_table["mudarie"] = "stateful" # "[muDArie]{.trn2}" # "stateful", resembling, prefixed
+map_table["mushar_il"] = "pointed-to noun" # "[muDArie]{.trn2}"
+map_table["mushtaqq"] = "deverbal" # deverbal, derived
+map_table["musnad"] = "structure-completer" # complement, leaning element
+map_table["musnadi"] = "structure-starter" # rock, mainstay, anchor, pillar, supporting element
+map_table["mustatir"] = "latent" # assumed, hidden, invisible, implied
+map_table["muxbar3"] = map_table["musnadi"] # theme,rock, mainstay, anchor, subject, keystone, cornerstone, post, mast, pillar
+map_table["muxbar"] = map_table["musnad"] # rheme, complement, predicate, cornerstone, keystone, capstone, rafter, beam
+map_table["mutaddi"] = "transitive"
+
+map_table["naib"] = "deputy"
+map_table["na3t"] = "attribute"
+map_table["nidaa"] = "vocative"
+map_table["naqis"] = "deficient"
+map_table["nasix"] = "canceling"
+
+map_table["participle"] = "participle"
+map_table["pbuh"] = "[ﷺ]{.aralt}"
+
+map_table["rabt"] = "link"
+
+map_table["sababi"] = "sababi" # takeover
+map_table["sahib_hal"] = "person of ḥāl"
+map_table["sahibs_hal"] = "persons of ḥāl"
+map_table["sifah"] = "adjectival noun"
+map_table["sifah_mush"] = "participle-like adjective" # {"participlish adjectival noun", nil} # "participle-like"?
+map_table["sighah_mub"] = "emphatic noun" # {"participlish adjectival noun", nil} # "participle-like"?
+map_table["silah"] = "connecting sentence"
+map_table["shart"] = "condition"
+map_table["shibh_fi3l"] = "quasi-verb"
+map_table["shibh_jumlah"] = "quasi-sentence"
+map_table["shibh_jumlah"] = "quasi-sentence"
+map_table["substantive"] = "substantive"
+
+map_table["taamm"] = "sufficient"
+map_table["tabi3"] = "follower"
+map_table["tamyiz"] = "[tamyIz]{.trn2}"
+map_table["tawkeed"] = "emphatic follower"
+
+map_table["xabar"] = "info" # newsworthy expression, information, comment, remark, info, report, news, expression
+
+map_table["zaaid"] = "redundant"
+map_table["zaahir"] = "overt"
+
+def get_sub_text(match):
+    global map_table
+    key = match.group()
+    #print(key)
+
+    # remove # which indicates to capitalize first letter
+    capitalize = False
+    if key[0] == '#':
+        assert(len(key) > 1)
+        capitalize = True
+        key = key[1:]
+
+    # remove ";"s
+    assert(len(key) > 2)
+    key = key[1:-1]
+
+    subbed_text = map_table[key] 
+    if capitalize:
+        subbed_text = subbed_text[0].upper() + subbed_text[1:] if len(subbed_text) > 1 else ""
+
+    return subbed_text
+
