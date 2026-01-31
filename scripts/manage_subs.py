52b6b94a5fb130d5231da95bc6db45fe0e89f076
adamiturabi <adamiturabi@adamiturabi-MacBook-Pro.local>
2026-01-31 19:54:44 +0500

    wip

--- scripts/manage_subs.py
+++ scripts/manage_subs.py
@@ -38,6 +38,7 @@ map_table["shaadh"] = "anomalous usage"
 map_table["taqdir"] = "virtual model" # "theoretical model" imaginary utterance, virtual, implication
 map_table["taqdiring"] = "virtual modeling" # "theoretical model" imaginary utterance, virtual, implication
 map_table["qaddar"] = "virtually model"
+map_table["qaddars"] = "virtually models"
 map_table["qaddared"] = "virtually modeled"
 map_table["Kuf"] = "[#kUf]{.trn2}"
 map_table["Basr"] = "[#baSr]{.trn2}"
