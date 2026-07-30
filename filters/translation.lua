48ab620d643bc930e8a1c47dd946639ddcefeb74
adamiturabi <adamiturabi@adamiturabi-MacBook-Pro.local>
2026-07-30 02:00:15 -0500

    Using guillemets for translations

--- filters/translation.lua
+++ filters/translation.lua
@@ -1,6 +1,7 @@
 Span = function (el)
   if el.classes:includes 'trans' then
-    return pandoc.Emph(el.content)
+    -- return pandoc.Emph(el.content)
+    return {"« "} .. el.content .. {" »"} -- no-break-spaces with guillemets
   end
 end
 
