16931c4bf715bfdcfa2741c67d597dca9bd1f947
adamiturabi <adamiturabi@adamiturabi-MacBook-Pro.local>
2026-08-02 01:44:20 -0500

    trying different terms for badal

--- scripts/manage_subs.py
+++ scripts/manage_subs.py
@@ -204,8 +204,8 @@ map_table["fi3l_qalb"] = "verb of perception"
 map_table["af3aal_quloob"] = "verbs of perception"
 map_table["amr"] = "command"
 
-map_table["badal"] = "re-render" # replacement, substitute, sub, rehash, restate, reword, do-over, re-word, re-render
-map_table["mubdalb"] = "pre-render" # replacee, substitutee, done-over
+map_table["badal"] = "sequel" # replacement, substitute, sub, rehash, restate, reword, do-over, re-word, re-render
+map_table["mubdalb"] = "prequel" # replacee, substitutee, done-over, pre-render
 
 map_table["damir_fasl"] = "separating-pronoun"
 map_table["damir_shan"] = "state-of-affairs pronoun" # pronoun-of-fact, pronoun of s͡haʾn
